8b5699e40adcf0061c36719fcf30ea7de49b8a6b
Simon Gerz <go69buj@mytum.de>
2024-06-29 17:12:19 +0200

    stash

---
+++ cmake-build-debug/CMakeFiles/gra24capsprojekt_t086.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gra24capsprojekt_t086.dir/Implementierung/main.c.o"
+  "CMakeFiles/gra24capsprojekt_t086.dir/Implementierung/main.c.o.d"
+  "gra24capsprojekt_t086"
+  "gra24capsprojekt_t086.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gra24capsprojekt_t086.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/gra24capsprojekt_t086.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for gra24capsprojekt_t086.
+# This may be replaced when dependencies are built.
