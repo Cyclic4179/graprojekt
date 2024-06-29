9f001a505abb45f803123df87f703d3ca2b392da
Pierre Klein <pierre@klein.red>
2024-06-29 23:16:31 +0200

    abortIfNULL: better error messages

--- Implementierung/util.h
+++ Implementierung/util.h
@@ -3,12 +3,14 @@
 #ifndef UTIL_GUARD
 #define UTIL_GUARD
 
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 inline void* __abort_null(void* value, char* desc) {
     if (!value) {
-        fprintf(stderr, "ERROR:\t\t'%s' was NULL", desc);
+        fprintf(stderr, "ERROR:\t\t'%s' was NULL (error: %s)", desc, strerror(errno));
         abort();
     }
     return value;
