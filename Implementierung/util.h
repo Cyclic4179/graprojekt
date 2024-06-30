60998af4104f9cb053f82585a891dfb9d214350a
Pierre Klein <pierre@klein.red>
2024-06-30 16:14:34 +0200

    util: better logging for abortIfNULL

--- Implementierung/util.h
+++ Implementierung/util.h
@@ -8,14 +8,25 @@
 #include <stdlib.h>
 #include <errno.h>
 
-inline void* __abort_null(void* value, char* desc) {
+inline void* __abort_null(void* value, char* desc, char* file, int line, char* msg) {
     if (!value) {
-        fprintf(stderr, "ERROR:\t\t'%s' was NULL (error: %s)", desc, strerror(errno));
+        fputs("ERROR:    ", stderr);
+        if (*msg != 0) {
+            fprintf(stderr, "%s: ", msg);
+        }
+        fprintf(stderr, "'%s' was NULL", desc);
+        if (errno != 0) {
+            fprintf(stderr, " (error: %s)", strerror(errno));
+        }
+        fprintf(stderr, " at %s, line %d\n", file, line);
         abort();
     }
     return value;
 }
 
-#define abortIfNULL(i) __abort_null(i, #i)
+#define abortIfNULL(i) __abort_null(i, #i, __FILE__, __LINE__, "")
+#define abortIfNULL_msg(i, j) __abort_null(i, #i, __FILE__, __LINE__, j)
+//#define abortIfNEQ(i,j) __abort_null(i-j, "#i - \\#j")
+//abortIfNEQ(i, j)
 
 #endif
