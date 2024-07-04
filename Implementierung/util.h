1f472c0b1452efb6c9bbbc7a02413e145685d16e
Pierre Klein <pierre@klein.red>
2024-07-04 15:11:10 +0200

    abortIfNULL: print functionname in which error occurred

--- Implementierung/util.h
+++ Implementierung/util.h
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 #include <errno.h>
 
-inline void* __abort_null(void* value, char* desc, char* file, int line, char* msg) {
+inline void* __abort_null(void* value, char* desc, const char* func, char* file, int line, char* msg) {
     if (!value) {
         fputs("ERROR:    ", stderr);
         if (*msg != 0) {
@@ -18,14 +18,14 @@ inline void* __abort_null(void* value, char* desc, char* file, int line, char* m
         if (errno != 0) {
             fprintf(stderr, " (error: %s)", strerror(errno));
         }
-        fprintf(stderr, " at %s, line %d\n", file, line);
+        fprintf(stderr, " in %s at %s, line %d\n", func, file, line);
         abort();
     }
     return value;
 }
 
-#define abortIfNULL(i) __abort_null(i, #i, __FILE__, __LINE__, "")
-#define abortIfNULL_msg(i, j) __abort_null(i, #i, __FILE__, __LINE__, j)
+#define abortIfNULL(i) __abort_null(i, #i, __func__, __FILE__, __LINE__, "")
+#define abortIfNULL_msg(i, j) __abort_null(i, #i, __func__, __FILE__, __LINE__, j)
 //#define abortIfNEQ(i,j) __abort_null(i-j, "#i - \\#j")
 //abortIfNEQ(i, j)
 
@@ -33,8 +33,9 @@ inline void* __abort_null(void* value, char* desc, char* file, int line, char* m
 #define DEBUG
 
 #ifdef DEBUG
-#define pdebug(...) fputs("DEBUG:    ", stderr); fprintf(stderr, __VA_ARGS__)
+#define pdebug(...) fprintf(stderr, "DEBUG:    " __VA_ARGS__)
 #define pdebug_(...) fprintf(stderr, __VA_ARGS__)
+//#define pdebug_var(...) fprintf(stderr, "DEBUG:    %s", #__VA_ARGS__)
 #else
 #define pdebug(...)
 #define pdebug_(...)
