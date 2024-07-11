9d2d2c9f9d28300f787e26ea086fcbd533980e38
Simon Gerz <go69buj@mytum.de>
2024-07-11 12:00:18 +0200

    restructured multiplication, removed duplicate code

--- Implementierung/mult.c
+++ Implementierung/mult.c
@@ -2,127 +2,24 @@
 #include "util.h"
 #include "ellpack.h"
 
-
-/// @brief main function
-/// @param a matrix a
-/// @param b matrix b
-/// @param result result of multiplication
-void matr_mult_ellpack(const void* a, const void* b, void* result) {
-    *(struct ELLPACK*)result = multiply_V1(*(struct ELLPACK*)a, *(struct ELLPACK*)b);
-
-    // ! DEBUG
-    const struct ELLPACK aE = *(struct ELLPACK*)a;
-    const struct ELLPACK bE = *(struct ELLPACK*)b;
-    struct ELLPACK res = *(struct ELLPACK*)result;
-
-    pdebug("== a ==\n");
-    pdebug("%lu\n", aE.noRows);
-    pdebug("%lu\n", aE.noCols);
-    pdebug("%lu\n", aE.maxNoNonZero);
-    pdebug("");
-    for (uint64_t i = 0; i < aE.noRows * aE.maxNoNonZero; i++) {
-        pdebug_("%f, ", aE.values[i]);
-    }
-    pdebug_("\n");
-    pdebug("");
-    for (uint64_t i = 0; i < aE.noRows * aE.maxNoNonZero; i++) {
-        pdebug_("%lu, ", aE.indices[i]);
-    }
-    pdebug_("\n\n");
-
-    pdebug("== b ==\n");
-    pdebug("%lu\n", bE.noRows);
-    pdebug("%lu\n", bE.noCols);
-    pdebug("%lu\n", bE.maxNoNonZero);
-    pdebug("");
-    for (uint64_t i = 0; i < bE.noRows * bE.maxNoNonZero; i++) {
-        pdebug_("%f, ", bE.values[i]);
-    }
-    pdebug("\n");
-    pdebug("");
-    for (uint64_t i = 0; i < bE.noRows * bE.maxNoNonZero; i++) {
-        pdebug_("%lu, ", bE.indices[i]);
-    }
-    pdebug_("\n\n");
-
-    pdebug("== o ==\n");
-    pdebug("%lu\n", res.noRows);
-    pdebug("%lu\n", res.noCols);
-    pdebug("%lu\n", res.maxNoNonZero);
-    pdebug("");
-    for (uint64_t i = 0; i < res.noRows * res.maxNoNonZero; i++) {
-        pdebug_("%f, ", res.values[i]);
-    }
-    pdebug_("\n");
-    pdebug("");
-    for (uint64_t i = 0; i < res.noRows * res.maxNoNonZero; i++) {
-        pdebug_("%lu, ", res.indices[i]);
-    }
-    pdebug_("\n");
-}
-
-/// @brief multiplies the matrices
-/// @param left left matrix
-/// @param right right matrix
-/// @result multiplied matrix
-struct ELLPACK multiply(struct ELLPACK left, struct ELLPACK right)
-{
-    // Array storing the amounts of non-zero entries per column in right    // TODO unused so far
-    /*
-    uint64_t* maxRowCounts = (uint64_t*)malloc(right.noRows * sizeof(uint64_t));
-    if (maxRowCounts == NULL) {
-        fprintf(stderr, "Error allocating memory");
-        abort();
-    }
-    for (uint64_t i = 0; i < right.noRows; i++) {
-        maxRowCounts[i] = 0;
-    }
-    for (uint64_t i = 0; i < right.noRows * right.maxNoNonZero; i++) {
-        if (right.values[i] != 0.f) {
-            maxRowCounts[right.indices[i]]++;
-        }
-    }
-    uint64_t maxRow = 0;                     // maximum number of non-zero entries of columns of right // TODO unused so far
-    for (uint64_t i = 0; i < right.noRows; i++) {
-        if (maxRowCounts[i] > maxRow) {
-            maxRow = maxRowCounts[i];
-        }
-    }
-    free(maxRowCounts);
-    */
-    // ############################## Start used code ##############################
-    // ############################## Setup: instantiating and memory allocation ##############################
-
+/// @brief main multiplication, first version
+/// @param a Pointer to left matrix
+/// @param b Pointer to right matrix
+/// @param res Pointer to result of multiplication
+void matr_mult_ellpack(const void* a, const void* b, void* res) {
+    const struct ELLPACK left = *(struct ELLPACK*)a;
+    const struct ELLPACK right = *(struct ELLPACK*)b;
     struct ELLPACK result;
-    result.noRows = left.noRows;
-    result.noCols = right.noCols;
-
-    uint64_t maxNoNonZeroLimit = (right.noCols > left.maxNoNonZero * right.maxNoNonZero) ? left.maxNoNonZero * right.maxNoNonZero : right.noCols;
-    result.maxNoNonZero = maxNoNonZeroLimit;      // TODO proven that this is correct yet
-
-    result.values = (float*)malloc(result.noRows * result.maxNoNonZero * sizeof(float));
-    result.indices = (uint64_t*)malloc(result.noRows * result.maxNoNonZero * sizeof(uint64_t));
-    if (result.values == NULL || result.indices == NULL) {
-        if (result.values != NULL) {
-            free(result.values);
-            fprintf(stderr, "Error allocating memory");
-        }
-        if (result.indices != NULL) {
-            free(result.indices);
-            fprintf(stderr, "Error allocating memory");
-        }
-        exit(EXIT_FAILURE);
-    }
+    result = initialize_result(left, right, result);
 
     uint64_t resultPos = 0;                  // pointer to next position to insert a value into result matrix
     uint64_t biggerDimension = (left.noRows > right.noCols) ? left.noRows : right.noCols;
 
-    // ############################## calculation of actual values ##############################
+    //########################################## calculation of actual values ##########################################
 
-    for (uint64_t i = 0; i < biggerDimension; i++) // Iterates over the rows of left
-    {
+    for (uint64_t i = 0; i < biggerDimension; i++) {// Iterates over the rows of left
+
         uint64_t leftRowIndex = i % left.noRows;
-
         for (uint64_t l = 0; l < right.noCols; l++) { // Iterates over the columns in the right matrix
 
             float sum = 0.f; // accumulator for an entry in result
@@ -130,10 +27,7 @@ struct ELLPACK multiply(struct ELLPACK left, struct ELLPACK right)
 
                 // leftColRightRow is the column index of the left and row index of the right matrix
                 uint64_t leftColRightRow = left.indices[leftRowIndex * left.maxNoNonZero + j];
-
-                // add here: if padded (= invalid value (float of 0.0)) then break
-
-                // Iterates over the column of right to check if value of right is not 0 / row of right has entry for that position
+                // Iterates over the column of right to check if row of right has entry for that position
                 for (uint64_t k = leftColRightRow * right.maxNoNonZero; k < (leftColRightRow + 1) * right.maxNoNonZero; k++) {
                     if (right.indices[k] == l) {
                         sum += left.values[leftRowIndex * left.maxNoNonZero + j] * right.values[k];
@@ -147,72 +41,29 @@ struct ELLPACK multiply(struct ELLPACK left, struct ELLPACK right)
             }
         }
         // add padding
-        for (; resultPos < (i + 1) * result.maxNoNonZero; resultPos++)
-        {
+        for (; resultPos < (i + 1) * result.maxNoNonZero; resultPos++) {
             result.values[resultPos] = 0.f;
             result.indices[resultPos] = 0;
         }
     }
-
-    // ############################## shrink result matrix by removing too long padding ##############################
-
-    uint64_t realResultMaxNoNonZero = 0;
-    uint64_t rowCounter;
-    for (uint64_t i = 0; i < result.noRows; i++) {
-        rowCounter = 0;
-        for (uint64_t j = 0; j < result.maxNoNonZero; ++j) {
-            if (result.values[result.maxNoNonZero * i + j] != 0.f) {
-                rowCounter++;
-            }
-        }
-        if (rowCounter > realResultMaxNoNonZero) {
-            realResultMaxNoNonZero = rowCounter;
-        }
-    }
-    uint64_t realResultPointer = 0;
-    for (uint64_t i = 0; i < result.noRows; i++) {
-        for (uint64_t j = 0; j < result.maxNoNonZero; j++) {
-            if (result.values[i * result.maxNoNonZero + j] != 0.f) {
-                result.values[realResultPointer] = result.values[i * result.maxNoNonZero + j];
-                result.indices[realResultPointer] = result.indices[i * result.maxNoNonZero + j];
-                realResultPointer++;
-            }
-        }
-        for (; realResultPointer < (i + 1) * realResultMaxNoNonZero; realResultPointer++)
-        {
-            result.values[realResultPointer] = 0.f;
-            result.indices[realResultPointer] = 0;
-        }
-    }
-    result.maxNoNonZero = realResultMaxNoNonZero;
-
-    return result;
+    *(struct ELLPACK*)res = remove_unnecessary_padding(result);
 }
 
-struct ELLPACK multiply_V1(struct ELLPACK left, struct ELLPACK right)
-{
-    // ############################## Setup: instantiating and memory allocation ##############################
-
+/// @brief main multiplication, second version
+/// @param a Pointer to left matrix
+/// @param b Pointer to right matrix
+/// @param res Pointer to result of multiplication
+void matr_mult_ellpack_V1(const void* a, const void* b, void* res) {
+    const struct ELLPACK left = *(struct ELLPACK*)a;
+    const struct ELLPACK right = *(struct ELLPACK*)b;
     struct ELLPACK result;
-    result.noRows = left.noRows;
-    result.noCols = right.noCols;
+    result = initialize_result(left, right, result);
 
-    uint64_t maxNoNonZeroLimit = (right.noCols > left.maxNoNonZero * right.maxNoNonZero) ? left.maxNoNonZero * right.maxNoNonZero : right.noCols;
-    result.maxNoNonZero = maxNoNonZeroLimit;      // TODO proven that this is correct yet
-
-    result.values = (float*)malloc(result.noRows * result.maxNoNonZero * sizeof(float));
-    result.indices = (uint64_t*)malloc(result.noRows * result.maxNoNonZero * sizeof(uint64_t));
     float* sum = malloc(right.noCols * sizeof(float));  // stores the products af a row of left with all columns of right
-    uint64_t* positions = malloc(right.noCols * sizeof(uint64_t));
-    if (result.values == NULL || result.indices == NULL || sum == NULL || positions == NULL) {
-        if (result.values != NULL) {
-            free(result.values);
-            fprintf(stderr, "Error allocating memory");
-        }
-        if (result.indices != NULL) {
-            free(result.indices);
-            fprintf(stderr, "Error allocating memory");
-        }
+    uint64_t* positions = malloc(right.noCols * sizeof(uint64_t));  // stores the associated indicies ot the values in sum
+    if (sum == NULL || positions == NULL) {
+        free(result.values);
+        free(result.indices);
         if (sum != NULL) {
             free(sum);
             fprintf(stderr, "Error allocating memory");
@@ -230,10 +81,9 @@ struct ELLPACK multiply_V1(struct ELLPACK left, struct ELLPACK right)
         sum[j] = 0.0;
     }
 
-    // ############################## calculation of actual values ##############################
+    //########################################## calculation of actual values ##########################################
 
-    for (uint64_t i = 0; i < biggerDimension; i++) // Iterates over the rows of left
-    {
+    for (uint64_t i = 0; i < biggerDimension; i++) {// Iterates over the rows of left
         uint64_t leftRowIndex = i % left.noRows;
 
         for (uint64_t j = 0; j < left.maxNoNonZero; j++) { // Iterates over a row of left
@@ -248,8 +98,7 @@ struct ELLPACK multiply_V1(struct ELLPACK left, struct ELLPACK right)
             }
         }
         // set value of result to calculated product
-        for (uint64_t j = 0; j < right.noCols; j++)
-        {
+        for (uint64_t j = 0; j < right.noCols; j++) {
             if (sum[j] != 0.0) {
                 result.indices[resultPos] = positions[j];
                 result.values[resultPos++] = sum[j];
@@ -265,9 +114,42 @@ struct ELLPACK multiply_V1(struct ELLPACK left, struct ELLPACK right)
     }
     free(sum);
     free(positions);
+    *(struct ELLPACK*)res = remove_unnecessary_padding(result);
+}
 
-    // ############################## shrink result matrix by removing too long padding ##############################
+/// @brief initialize the result matrix, check for multiplicable dimensions
+/// @param left left matrix
+/// @param right right matrix
+/// @param result result matrix
+/// @result initialized result matrix
+struct ELLPACK initialize_result(struct ELLPACK left, struct ELLPACK right, struct ELLPACK result) {
+    if (left.noCols != right.noRows) {
+        fprintf(stderr, "Error: the given matrices do not have multiplicable dimensions.");
+        exit(EXIT_FAILURE);
+    }
+    result.noRows = left.noRows;
+    result.noCols = right.noCols;
+    result.maxNoNonZero = (right.noCols > left.maxNoNonZero * right.maxNoNonZero) ? left.maxNoNonZero * right.maxNoNonZero : right.noCols; //Proven by Pierre that this limit is correct
+    result.values = (float*)malloc(result.noRows * result.maxNoNonZero * sizeof(float));
+    result.indices = (uint64_t*)malloc(result.noRows * result.maxNoNonZero * sizeof(uint64_t));
+    if (result.values == NULL || result.indices == NULL) {
+        if (result.values != NULL) {
+            free(result.values);
+            fprintf(stderr, "Error allocating memory");
+        }
+        if (result.indices != NULL) {
+            free(result.indices);
+            fprintf(stderr, "Error allocating memory");
+        }
+        exit(EXIT_FAILURE);
+    }
+    return result;
+}
 
+/// @brief remove unnecessary padding in the result matrix and // still TODO: free the unused memory
+/// @param result result matrix
+/// @result smaller matrix
+struct ELLPACK remove_unnecessary_padding(struct ELLPACK result) {
     uint64_t realResultMaxNoNonZero = 0;
     uint64_t rowCounter;
 
@@ -300,6 +182,45 @@ struct ELLPACK multiply_V1(struct ELLPACK left, struct ELLPACK right)
     }
     result.maxNoNonZero = realResultMaxNoNonZero;
     return result;
+}
+
+/// @brief output debug information
+/// @param left left matrix
+/// @param right right matrix
+/// @param result result matrix
+void debug_info(struct ELLPACK left, struct ELLPACK right, struct ELLPACK result) {
+    pdebug("== left ==\n");
+    pdebug("%lu, %lu, %lu\n", left.noRows, left.noCols, left.maxNoNonZero);
+    for (uint64_t i = 0; i < left.noRows * left.maxNoNonZero; i++) {
+        pdebug_("%f, ", left.values[i]);
+    }
+    pdebug_("\n");
+    for (uint64_t i = 0; i < left.noRows * left.maxNoNonZero; i++) {
+        pdebug_("%lu, ", left.indices[i]);
+    }
+    pdebug_("\n\n");
+
+    pdebug("== right ==\n");
+    pdebug("%lu, %lu, %lu\n", right.noRows, right.noCols, right.maxNoNonZero);
+    for (uint64_t i = 0; i < right.noRows * right.maxNoNonZero; i++) {
+        pdebug_("%f, ", right.values[i]);
+    }
+    pdebug("\n");
+    for (uint64_t i = 0; i < right.noRows * right.maxNoNonZero; i++) {
+        pdebug_("%lu, ", right.indices[i]);
+    }
+    pdebug_("\n\n");
+
+    pdebug("== result ==\n");
+    pdebug("%lu, %lu, %lu\n", result.noRows, result.noCols, result.maxNoNonZero);
+    for (uint64_t i = 0; i < result.noRows * result.maxNoNonZero; i++) {
+        pdebug_("%f, ", result.values[i]);
+    }
+    pdebug_("\n");
+    for (uint64_t i = 0; i < result.noRows * result.maxNoNonZero; i++) {
+        pdebug_("%lu, ", result.indices[i]);
+    }
+    pdebug_("\n");
 }
 /*
  * Step 1: read matrices
--- Implementierung/mult.h
+++ Implementierung/mult.h
@@ -6,19 +6,30 @@
 #include <stdio.h>
 
 
-#define MAX_IMPL_VERSION 0
+#define MAX_IMPL_VERSION 1
 
 /// @brief main function
 /// @param a matrix a
 /// @param b matrix b
 /// @param result result of multiplication
 void matr_mult_ellpack(const void* a, const void* b, void* result);
+void matr_mult_ellpack_V1(const void* a, const void* b, void* result);
 
-/// @brief multiplies the matrices
+/// @brief initialize the result matrix, check for multiplicable dimensions
 /// @param left left matrix
 /// @param right right matrix
-/// @result multiplied matrix
-struct ELLPACK multiply(struct ELLPACK left, struct ELLPACK right);
-struct ELLPACK multiply_V1(struct ELLPACK left, struct ELLPACK right);
+/// @param result result matrix
+/// @result initialized result matrix
+struct ELLPACK initialize_result(struct ELLPACK left, struct ELLPACK right, struct ELLPACK result);
+
+/// @brief remove unnecessary padding in the result matrix and free the unused memory
+/// @param result result matrix
+struct ELLPACK remove_unnecessary_padding(struct ELLPACK result);
+
+/// @brief output debug information
+/// @param left left matrix
+/// @param right right matrix
+/// @param result result matrix
+void debug_info(struct ELLPACK left, struct ELLPACK right, struct ELLPACK result);
 
 #endif
