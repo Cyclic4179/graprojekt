9decee5c65dc523c2dd6767e744d8324fea1bba8
Pierre Klein <pierre@klein.red>
2024-07-13 14:58:01 +0200

    gen_input.py: fix

--- Implementierung/tests/gen_input.py
+++ Implementierung/tests/gen_input.py
@@ -59,7 +59,6 @@ def create(
     no_cols: int,
     no_non_zero: int,
     max_val: int,
-    sparsity: int,
     use_floats: bool,
 ):
     """Creates a random sparse matrix
@@ -70,10 +69,10 @@ def create(
         no_cols (int): number of columns
         no_non_zero (int): maximal number of non zero values per row
         max_val (int): highest possible value
-        sparsity (int): Every sparsity. value remains empty
         use_floats (bool): should floats be used as values
     """
-    assert no_non_zero < no_cols
+    # TODO: maybe some argument to control sparsity
+    assert no_non_zero <= no_cols
 
     dest = gen_dir.joinpath(dest)
     dest.parent.mkdir(parents=True, exist_ok=True)
@@ -81,89 +80,55 @@ def create(
     s_float = ""
     s_index = ""
 
-    wrote_max_row = False
+    # index of row that will have max nonzero entries
+    row_with_max = randint(0, no_rows - 1)
 
     # strategy: start at random index, go forth with random steps
     # and if too large or already filled no_non_zero entries go to
     # next line, repeat
-    for _ in range(no_rows):
+    # also, ensure there exists at least one line with no_non_zero nonzero entries
+    for i in range(no_rows):
+
+        if i == row_with_max:
+            # row will be max
+            for k in range(no_non_zero):
+                s_float += f"{gen_random_value(max_val, use_floats)},"
+                s_index += f"{k},"
+            continue
+
         cur_row = randint(0, no_cols)
         j = 0
-        #print("\tbatch start")
 
         # this is one row
         while True:
             if cur_row > no_cols - 1 or j == no_non_zero:
                 rem_cells = no_non_zero - j
-                #print("cur_row:", cur_row)
-                #print("j:", j)
-                #print("rem_cells:", rem_cells)
 
-                if rem_cells <= 0:
-                    #print("aaaa")
-                    wrote_max_row = True
-
-                else:
+                if rem_cells > 0:
                     s = "*," * rem_cells # fill with remaining '*,'
-                    #print("s:\t", s)
                     s_float += s
                     s_index += s
 
                 break
 
             s_float += f"{gen_random_value(max_val, use_floats)},"
-            #print("fchange:", s_float)
             s_index += f"{cur_row},"
             j += 1
 
             if cur_row == no_cols - 1:
                 rem_cells = no_non_zero - j
-                #print("cur_row:", cur_row)
-                #print("j:", j)
-                #print("rem_cells:", rem_cells)
 
-                if rem_cells <= 0:
-                    #print("aaaa")
-                    wrote_max_row = True
-
-                else:
+                if rem_cells > 0:
                     s = "*," * rem_cells # fill with remaining '*,'
-                    #print("s:\t", s)
                     s_float += s
                     s_index += s
+
                 break
 
             cur_row += randint(1, no_cols - cur_row) # next index
 
-    if not wrote_max_row:
-        chr_to_rem = 2 * no_non_zero
-        #print(s_float)
-        #print(s_index)
-        s_float = s_float[:-chr_to_rem]
-        s_index = s_index[:-chr_to_rem]
-        #print(s_float)
-        #print(s_index)
-
-        for i in range(no_non_zero):
-            s_float += f"{gen_random_value(max_val, use_floats)},"
-            s_index += f"{i},"
-
     s_float = s_float[:-1]
     s_index = s_index[:-1]
-
-    #s_float += self.value(max, floats)
-    #s_index += self.index(noCols,previous + 1)
-
-    #for i in range(itemsCount - 1):
-    #    if emptyLine2[i]:
-    #        #f.write("*,")
-    #        index = 0
-    #    else:
-    #        noRowsAfter = noNonZero - (i % noNonZero) - 1
-    #        val = self.index(noCols - noRowsAfter, index + 1)
-    #        f.write(f"{val},")
-    #        index = 0 if noRowsAfter == 0 else val
-
 
     with open(str(dest), "w", encoding="ascii") as f:
         f.write(f"{no_rows},{no_cols},{no_non_zero}\n"
@@ -172,26 +137,26 @@ def create(
 
 
 def main():
-    create_a("1", 4, 4, 2, 99, 3, False)
-    create_b("1", 4, 4, 2, 99, 3, False)
+    create_a("1", 1, 100, 80, 99, False)
+    create_b("1", 100, 4, 2, 99, False)
 
-    create_a("2", 10000, 10000, 400, 99, 3, False)
-    create_b("2", 10000, 10000, 200, 99, 3, False)
+    #create_a("2", 10000, 10000, 400, 99, False)
+    #create_b("2", 10000, 10000, 200, 99, False)
 
-    create_a("3", 100, 100, 80, 99, 3, False)
-    create_b("3", 100, 100, 80, 99, 3, False)
+    create_a("3", 100, 100, 80, 99, False)
+    create_b("3", 100, 100, 80, 99, False)
 
-    #create_a("3", 9, 9, 4, MAX_VAL, 3, False)
-    #create_b("3", 9, 9, 4, MAX_VAL, 3, False)
+    create_a("4", 9, 9, 4, MAX_VAL, False)
+    create_b("4", 9, 9, 4, MAX_VAL, False)
 
-    #create_a("4", 4, 4, 2, 99, 3, True)
-    #create_b("4", 4, 4, 2, 99, 3, True)
+    create_a("5", 4, 4, 2, 99, True)
+    create_b("5", 4, 4, 2, 99, True)
 
-    #create_a("5", 9, 9, 4, 99, 3, True)
-    #create_b("5", 9, 9, 4, 99, 3, True)
+    create_a("6", 9, 9, 4, 99, True)
+    create_b("6", 9, 9, 4, 99, True)
 
-    #create_a("6", 9, 9, 4, MAX_VAL, 3, True)
-    #create_b("6", 9, 9, 4, MAX_VAL, 3, True)
+    create_a("7", 9, 9, 4, MAX_VAL, True)
+    create_b("7", 9, 9, 4, MAX_VAL, True)
 
 
 if __name__ == "__main__":
